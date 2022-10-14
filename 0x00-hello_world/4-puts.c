3c473f94d3ca339b41628acc595260f7f59d2d72
salaamolushola77 <salaamolushola01@gmail.com>
2022-10-13 21:16:51 -0700

    prints exact statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  *
  * Return: By default returns zero if no errors otherwise returns non-zero value
  */
-int main()
+int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
