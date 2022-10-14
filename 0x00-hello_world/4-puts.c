ebb0a7817003095e65cf9ad882d2349e6eb21a09
salaamolushola77 <salaamolushola01@gmail.com>
2022-10-13 21:03:21 -0700

    prints exact statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
  * main- Serves as the entry point for the program
  *
  * Return: By default returns zero if no errors otherwise returns non-zero value.
  */
-int()
+int main()
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
