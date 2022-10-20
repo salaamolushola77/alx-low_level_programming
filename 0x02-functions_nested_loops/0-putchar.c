9108c59eae97fcea7629b314f348bb9ea76bc77a
salaamolushola77 <salaamolushola01@gmail.com>
2022-10-19 18:10:06 -0700

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x[8] = {'_','p','u','t','c','h','a'.'r')
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(x[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
