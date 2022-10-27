48ff08ed317e3aa6354d7c0e9940d31b13847e83
salaamolushola77 <salaamolushola01@gmail.com>
2022-10-26 17:55:35 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - Prints Holbeerton as a message
+ * main - Entry point
  *
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
 
-	sz = sizeof(str)/sizeof(int);
-	for (count = o; count < sz; count++)
-	{
-		_putchar(str[count]);
-	}
-	_putchar("\n");
+int main (void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
 	return (0);
 }
