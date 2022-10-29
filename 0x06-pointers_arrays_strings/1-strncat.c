4bf85f4c90afecb4aeb62ae7b0300d6e0a98d309
salaamolushola77 <salaamolushola01@gmail.com>
2022-10-28 21:03:17 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,34 @@
 #include <main.h>
 
 /**
- * _strncat - concatenates two strings using at most
- * 	      an inputed numbr of byte from src.
- *@dest: The string is to be appended upon.
- *@src: The string to be appeended to dest.
- *n: The number of bytes from src to be appended to dest.
+ * _strncat - concatenates two strings
  *
- * Return: A pointer to the resulting string dest.
+ *@dest: variable to hold the first string.
+ *@src: variable to hold second string
+ *n: variable to hold limit to impose on the second string.
+ *
+ * Return: Always 0 (success).
  */
 char *_strncat(char *dest, char *src, int n);
+
 {
-	int index = 0, dest_len = 0;
+
+	int index, dest_len;
+
+	index = dest_len = 0;
+
 
 	while (dest[index++])
+
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
+
 		dest[dest_len++] = src[index];
 
+
+
 	return (dest);
+/*Only difference with the first code is the n condition*/
+
 }
