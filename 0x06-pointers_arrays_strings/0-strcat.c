f08086903076f8caaa29d00be7dc8371d06791ae
salaamolushola77 <salaamolushola01@gmail.com>
2022-10-28 21:19:20 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,32 +8,54 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
+
 {
-	int count = 0, count2 = 0;
 
-	while (*(dest + count) != '\0')
+	int i;
+
+	int j;
+
+
+
+	i = 0;
+
+
+
+	while (dest[i] != '\0')
+
+
 
 	{
 
-		count++;
+
+
+		i++;
+
+
 
 	}
 
-	while (count2 >= 0)
+	j = 0;
+
+	while (src[j] != '\0')
+
+
 
 	{
 
-		*(dest + count) = *(src + count2);
+		dest[i] = src[j];
 
-		if (*(src + count2) == '\0')
+		i++;
 
-		break;
+		j++;
 
-		count +
 
-		count2++;
 
 	}
-		return (dest);
+
+	dest[i] = '\0';
+
+	return (dest);
+
 }
