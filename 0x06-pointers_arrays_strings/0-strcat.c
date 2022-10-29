4c93bb11e3f70103ede301181c4a694b07843f11
salaamolushola77 <salaamolushola01@gmail.com>
2022-10-28 20:42:43 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,12 +10,30 @@
  */
 char *_strcat(char *dest, char *src);
 {
-	int index = 0, dest_len = 0;
+	int count = 0, count2 = 0;
 
-	while (dest[indx++])
-		dest len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (*(dest + count) != '\0')
 
-	return (dest);
-}
+	{
+
+		count++;
+
+	}
+
+	while (count2 >= 0)
+
+	{
+
+		*(dest + count) = *(src + count2);
+
+		if (*(src + count2) == '\0')
+
+			break;
+		count +
+
+			count2++;
+
+		{
+			return (dest);
+
+		}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,7 @@
  *
  * Header file contains all the prototypes for 0x06-pointers_arrays_strings
  */
+
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -12,5 +13,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
 
